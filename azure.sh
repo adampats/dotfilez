2556af7ae6e85b492fdbaf16b42653893cb9bd12
adampats <adampatterson@protonmail.com>
2019-03-21 22:25:39 -0700

    Azure

---
+++ azure.sh
@@ -0,0 +1,5 @@
+### Azure
+
+azure_unset_env_vars () {
+  unset ARM_ACCESS_KEY ARM_CLIENT_ID ARM_CLIENT_SECRET ARM_SUBSCRIPTION_ID ARM_TENANT_ID
+}
--- functions.sh
+++ functions.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/general.sh"
 source "$DIR/aws.sh"
+source "$DIR/azure.sh"
 source "$DIR/docker.sh"
 
 ### git
